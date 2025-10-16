d36ec398fc95e396fc8ee7ec4b79300cb63e1c9a
Chase McLain <chasermclain@gmail.com>
2025-10-16 15:35:09 -0600

    ingredient model

---
+++ server/Models/Ingredient.cs
@@ -0,0 +1,12 @@
+namespace all_state.Models;
+
+
+public class Ingredient
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public string Name { get; set; }
+  public string Quantity { get; set; }
+  public int RecipeId { get; set; }
+}
