38dc8b4e7a549c7aafb0fdcadc79600ddeadbe37
Chase McLain <chasermclain@gmail.com>
2025-10-16 15:20:12 -0600

    recipes repo

--- server/Services/RecipesService.cs
+++ server/Services/RecipesService.cs
@@ -6,10 +6,12 @@ namespace all_state.Services;
 public class RecipesService
 {
 
+  private readonly RecipesRepository _recipesRepository;
 
-
-
-
+  public RecipesService(RecipesRepository recipesRepository)
+  {
+    _recipesRepository = recipesRepository;
+  }
 
 
 
@@ -17,3 +19,5 @@ public class RecipesService
 
 
 }
+
+
