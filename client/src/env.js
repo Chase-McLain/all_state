ff57fbd2f28ac66b45e41d2034e642229ea1466c
Chase McLain <chasermclain@gmail.com>
2025-10-16 14:32:31 -0600

    recipe table created

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'dev-gdmi1jflr5z60pgv.us.auth0.com'
 export const clientId = '5fFnhGltXZVbQ73tZa24zPc5wQOQ0jDY'
