8307730283285bccbfb1ff34b8e9074a60534fdc
Chase McLain <chasermclain@gmail.com>
2025-10-16 15:11:31 -0600

    recipes controller

---
+++ server/Controllers/RecipesController.cs
@@ -0,0 +1,34 @@
+namespace all_state.controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+
+
+public class RecipesController : ControllerBase
+{
+
+
+
+
+  // [HttpGet]
+  // public List<Recipe> GetRecipes()
+  // {
+  //   try
+  //   {
+  //     List<Recipe> recipes = 
+  //     return recipes;
+  //   }
+  //   catch (Exception exception)
+  //   {
+  //     BadRequest(exception);
+  //   }
+  // }
+
+
+
+
+
+
+
+}
