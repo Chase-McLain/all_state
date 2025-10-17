6e38c4bc722477d49ac6390484d0904e029e32a1
Chase McLain <chasermclain@gmail.com>
2025-10-17 15:34:22 -0600

    recipe model requirements

--- server/Models/Recipe.cs
+++ server/Models/Recipe.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace all_state.Models;
 
 
@@ -6,9 +8,9 @@ public class Recipe
   public int Id { get; set; }
   public DateTime CreatedAt { get; set; }
   public DateTime UpdatedAt { get; set; }
-  public string Title { get; set; }
-  public string Instructions { get; set; }
-  public string Img { get; set; }
+  [MinLength(1), MaxLength(255)] public string Title { get; set; }
+  [MaxLength(5000)] public string Instructions { get; set; }
+  [Url, MinLength(1), MaxLength(1000)] public string Img { get; set; }
   public string Category { get; set; }
   public string CreatorId { get; set; }
   public Profile Creator { get; set; }
