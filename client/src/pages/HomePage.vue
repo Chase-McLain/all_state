484245ff53874e7add54288d0e97bdc6fb9794f2
Chase McLain <chasermclain@gmail.com>
2025-10-21 15:21:54 -0600

    recipe model client side

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,11 +1,39 @@
 <script setup>
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
+
+
+
+
+
+onMounted(() => {
+
+})
+
+
+async function getRecipes() {
+  try {
+    //
+  }
+  catch (error) {
+    Pop.error(error);
+    logger.error(error)
+  }
+}
 
 
 
 </script>
 
 <template>
-  All state
+  <main class="container">
+    <section class="row">
+      <div class="col">
+        recipes
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped lang="scss"></style>
